25fa62b48d9b7cbfc68a6288759dc705a04c9ea3
Carlos Tapasco <carlos.tapasco@wizeline.com>
2023-05-29 22:32:34 -0500

    Day 01 exercises 2 and 3

---
+++ day_01/distance.py
@@ -0,0 +1,19 @@
+# Find the Euclidian distance between (2, 3) and (10, 8)
+
+from math import dist
+
+p = (2, 3)
+q = (10, 8)
+
+print(dist(p,q))
+
+# Write an example for different Python data types such as Number (Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary
+print(type(84))                  # Int
+print(type(17.35))               # Float
+print(type(3 + 18j))             # Complex number
+print(type('Matias'))            # String
+print(type(True))                # Boolean
+print(type(['Marcela', 'UNAD', 'Cali']))           # List
+print(type((2, 4, 8)))             # Tuple
+print(type({'Marcela', 'Ramirez', 'Arias'}))           # Set
+print(type({'name': 'Matias', 'lastname': 'Tapasco'}))          # Dictionary
