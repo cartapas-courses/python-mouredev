ed946c3754bb15fc34bf878de6404a6734b7bfc4
Carlos Tapasco <carlos.tapasco@wizeline.com>
2023-05-29 22:32:14 -0500

    Day 01 exercise 1

---
+++ day_01/helloworld.py
@@ -0,0 +1,32 @@
+# Exercises Day 1 - 30DaysOfPython Challenge
+
+# Check the python version
+# https://note.nkmk.me/en/python-sys-platform-version-info/
+import sys
+print(sys.version)
+
+# Math operations
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 % 4)             # modulus(%)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 // 4)            # Floor division operator(//)
+
+# Write strings
+print('Carlos')          # Your name
+print('Tapasco')         # Your family name
+print('Colombia')        # Your country
+print('I am enjoying 30 days of python')
+
+
+# Checking data types
+print(type(10))                  # Int
+print(type(9.8))                 # Float
+print(type(3.14))                # Float
+print(type(4 - 4j))              # Complex number
+print(type(['Carlos', 'Python', 'Colombia']))           # List
+print(type('Carlos'))            # String
+print(type('Tapasco'))           # String
+print(type('Colombia'))          # String
