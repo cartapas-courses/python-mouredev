1d2fe8f873cf578f78174bf89e0331884e17c6cc
Carlos Andres Tapasco Viera <cartapas@gmail.com>
2023-06-13 09:38:44 -0500

    New style string formatting

---
+++ day_04/new_string_formatting.py
@@ -0,0 +1,16 @@
+first_name = 'Matias'
+last_name = 'Tapasco'
+language = 'Python'
+formatted_string = 'My son is {} {}. He would like to study {}'.format(first_name, last_name, language)
+print(formatted_string)
+
+a = 4
+b = 3
+
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b))
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
